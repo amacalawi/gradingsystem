fb1d5c2cc98bb72fb9d51987c6071ee6712f3102
root <amacalawi@gmail.com>
2020-08-27 13:03:51 +0800

    infoblast-02-01

---
+++ resources/views/modals/infoblast-template.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="infoblast-template" tabindex="-1" role="dialog" aria-labelledby="infoblastTemplate">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-accent">
+                <h5 class="modal-title full-width c-white" id="infoblastTemplate">
+                    <span class="infoblast-template-title">Insert Messaging Template</span>
+                    <a href="javascript:;" class="pull-right c-white" data-dismiss="modal">
+                        <i class="la la-close"></i>
+                    </a>
+                </h5>
+            </div>
+            <div class="modal-body">
+                <div class="infoblast-template-layer">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
